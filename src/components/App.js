ea865d7f284000918cb6dc10d172624baf48e255
nismojkee <stanejkee@gmail.com>
2021-02-28 20:55:12 +0300

    some fixed style

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
+
+import Main from './Main';
+import Blog from './Blog/Blog'
 import ProductList from './ProductList';
 import Details from './Details';
 import Cart from './Cart';
@@ -13,7 +16,9 @@ class App extends Component {
 			<React.Fragment>
 				<Navbar />
 				<Switch>
-					<Route path="/" exact component={ProductList} />
+					<Route path="/" exact component={Main} />
+					<Route path="/blog" component={Blog} />
+					<Route path="/catalog" component={ProductList} />
 					<Route path="/cart" component={Cart} />
 					<Route path="/details" component={Details} />
 					<Route component={Default} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 
 class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar position-sticky sticky-top navbar-expand-sm navbar-dark px-sm-5 bg-dark shadow-sm">
+			<>
+			<nav className="navbar position-sticky sticky-top navbar-expand-md navbar-dark px-sm-5 bg-dark shadow-sm">
 				<Link to="/"><span className="navbar-brand">eShop</span></Link>
 
-				<ul className="navbar-nav ml-auto">
-					<li className="nav-item">
-						<NavLink to="/cart" activeClassName="active" className="nav-link">
-							<i className="fa fa-shopping-cart"></i> Cart <span className="badge bg-light text-dark">0</span>
-						</NavLink>
-					</li>
-				</ul>
+					<button className="navbar-toggler" type="button" >
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<NavLink to="/catalog" activeClassName="active" className="nav-link">
+								Catalog
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink to="/blog" activeClassName="active" className="nav-link">
+								News
+							</NavLink>
+						</li>
+					</ul>
+
+					<ul className="navbar-nav ml-auto">
+						<form className="form-inline mr-3">
+							<input type="search" className="form-control form-control-sm" placeholder="Search..."/>
+						</form>
+						<li className="nav-item">
+							<NavLink to="/cart" activeClassName="active" className="nav-link">
+								<i className="fa fa-shopping-cart mr-2"></i>
+								Cart
+								<ProductConsumer>
+									{value => {
+										const { cart } = value;
+										const cartLength = cart.length;
+										return ( <span className="badge bg-light text-dark ml-2">{cartLength}</span>)
+									}}
+								</ProductConsumer>
+							</NavLink>
+						</li>
+					</ul>
+				</div>
 			</nav>
+			</>
 		)
 	}
 }
